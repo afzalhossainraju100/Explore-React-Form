e4e5a34b6aaf4e44af2d88c6bacffe42805e0cdf
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-12-29 23:19:33 +0600

    Handle all error

--- src/components/ControledField/ControlField.jsx
+++ src/components/ControledField/ControlField.jsx
@@ -1,25 +1,49 @@
 import React, { useState } from "react";
 
 const ControlField = () => {
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form submitted");
+
+    if (password.length < 6) {
+      setError("Password must be at least 6 characters long");
+      return;
+    }
+    else{
+        setError("");
+    }
+
+    console.log("Form submitted successfully", { email, password });
   };
-  console.log(password);
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="email" placeholder="Email" required />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
         <br />
         <input
           type="password"
           placeholder="Password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            e.target.value.length < 6 
+              ? setError("Password must be at least 6 characters long") 
+              : setError("");
+          }}
           required
         />
         <br />
+        {error && <p style={{ color: "red" }}>{error}</p>}
         <input type="submit" value="Submit" />
       </form>
     </div>
